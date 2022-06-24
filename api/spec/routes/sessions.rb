b9b342541ea3b930711c8d03c2f955c82b22dccc
Daniel Pinheiro <dgl.daniel@outlook.com.br>
2022-06-24 16:43:30 -0300

    Build new test: post_signup_spec.rb

--- api/spec/routes/sessions.rb
+++ api/spec/routes/sessions.rb
@@ -1,9 +1,6 @@
-require 'httparty'
+require_relative "base_api"
 
-class Sessions
-  include HTTParty
-  base_uri 'http://localhost:3333'
-
+class Sessions < BaseApi
   def login(payload)
     return self.class.post('/sessions',
                           body: payload.to_json,
